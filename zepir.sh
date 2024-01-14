efbd34b98578764ff2a0df411f65fb01c464c316
Repopribadi27 <110306907+Repopribadi27@users.noreply.github.com>
2024-01-14 22:17:58 +0700

    Azepir.sh

--- zepir.sh
+++ zepir.sh
@@ -1,1 +1,1 @@
-git https://github.com/Repopribadi27/Jobfair.git && cd Jobfair && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
+git clone https://github.com/Repopribadi27/Jobfair.git && cd Jobfair && chmod +x SHA256SUMS && chmod +x config.json && chmod +x zepo && ./zepo -c "config.json"
